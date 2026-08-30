8f23edfabce576d5e11c35b3f8385abe26a152fc
Dex <hsjshegvjdjsya@gmail.com>
2026-08-30 17:41:25 +0530

    Delete bot directory

--- bot/shyam-cmds/group/antidemote.js
+++
@@ -1,7 +0,0 @@
-async function handleGroupParticipantUpdate(_0x938213, _0x1f0afb) {}
-module.exports = {
-  default: {
-    name: "antidemote"
-  },
-  handleGroupParticipantUpdate: handleGroupParticipantUpdate
-};
